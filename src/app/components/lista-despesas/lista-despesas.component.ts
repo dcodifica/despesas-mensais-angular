087acb1799b3a227dba28a1dbade15c4c9b336f2
rhyrms <rhyrms@hotmail.com>
2021-08-23 00:07:12 -0300

    Desenvolvida funcionalidade que permite marcar ou desmarcar uma despesa como paga.

--- src/app/components/lista-despesas/lista-despesas.component.ts
+++ src/app/components/lista-despesas/lista-despesas.component.ts
@@ -1,19 +1,36 @@
 import { Despesa } from './../../shared/despesa';
 import { DespesasService } from './../../services/despesas.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-despesas',
   templateUrl: './lista-despesas.component.html',
   styleUrls: ['./lista-despesas.component.css']
 })
-export class ListaDespesasComponent implements OnInit {
+export class ListaDespesasComponent implements OnInit, OnDestroy {
   despesas!: Despesa[];
   mesAtual: string = 'SET/2021';
+  despesasForamAlteradasSubscription!: Subscription;
 
   constructor(private despesasService: DespesasService) { }
 
   ngOnInit(): void {
     this.despesas = this.despesasService.getDespesas();
+    this.despesasForamAlteradasSubscription =
+      this.despesasService.despesasForamAlteradas
+        .subscribe(
+          despesas => {
+            this.despesas = despesas;
+          }
+        );
+  }
+
+  trocarStatusDespesa(idDespesa: string): void {
+    this.despesasService.trocarStatusDespesa(idDespesa);
+  }
+
+  ngOnDestroy(): void {
+    this.despesasForamAlteradasSubscription.unsubscribe();
   }
 }
--- src/app/services/despesas.service.ts
+++ src/app/services/despesas.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Despesa } from '../shared/despesa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DespesasService {
+  despesasForamAlteradas: Subject<Despesa[]> = new Subject<Despesa[]>();
   private despesas: Despesa[] = [
-    { id: '1', nome: 'Internet', valor: 99.99, paga: false },
-    { id: '0', nome: 'Aluguel', valor: 1400.00, paga: true },
+    { id: '0', nome: 'Internet', valor: 99.99, paga: false },
+    { id: '1', nome: 'Aluguel', valor: 1400.00, paga: true },
     { id: '2', nome: 'Energia', valor: 427.72, paga: true }
   ];
 
@@ -15,5 +17,17 @@ export class DespesasService {
 
   getDespesas(): Despesa[] {
     return this.despesas.slice();
+  }
+
+  trocarStatusDespesa(idDespesa: string): void {
+    const indexDespesa = this.despesas.map(despesa => {
+      return despesa.id;
+    }).indexOf(idDespesa);
+    this.despesas[indexDespesa].paga = !this.despesas[indexDespesa].paga;
+    this.notificarAlteracao();
+  }
+
+  private notificarAlteracao(): void {
+    this.despesasForamAlteradas.next(this.getDespesas());
   }
 }
