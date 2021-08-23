9de0864f2cf9d68e2284653dd6295479aa688227
rhyrms <rhyrms@hotmail.com>
2021-08-23 19:32:09 -0300

    Criada estrutura do form de cadastro de despesa.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { FormDespesaComponent } from './components/form-despesa/form-despesa.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form-despesa/form-despesa.component.ts
+++ src/app/components/form-despesa/form-despesa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form-despesa',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-despesa.component.css']
 })
 export class FormDespesaComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
